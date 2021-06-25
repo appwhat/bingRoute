a369eca22dba18cd2fd29e5bbdea2d94951bab36
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2021-06-25 13:12:10 -0300

    Add files via upload

---
+++ myScript.js
@@ -0,0 +1,21 @@
+var caixaText = document.getElementById('caixaText')
+
+var shif = document.getElementById('shif')
+
+onkeydown = (e) => {
+    if (e.shiftKey) {
+        if (caixaText.style.display == "none") {
+            caixaText.style.display = "block"
+        } else {
+            caixaText.style.display = "none"
+        }
+    }
+}
+
+shif.onclick = () => {
+    if (caixaText.style.display == "none") {
+        caixaText.style.display = "block"
+    } else {
+        caixaText.style.display = "none"
+    }
+}
