eacaec221a633b5bce6a9b83d961811c8dae1d2b
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2021-07-03 09:02:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 var map, infobox, dataLayer, directionsManager;
 
 //Query URL to the Fourth Coffe Shop data source
-var sdsDataSourceUrl = 'http://spatial.virtualearth.net/REST/v1/data/20181f26d9e94c81acdf9496133d4f23/FourthCoffeeSample/FourthCoffeeShops';
+/* var sdsDataSourceUrl = 'http://spatial.virtualearth.net/REST/v1/data/20181f26d9e94c81acdf9496133d4f23/FourthCoffeeSample/FourthCoffeeShops'; */
 
 function GetMap() {
     map = new Microsoft.Maps.Map('#myMap', {});
-
 
 
     //Create a layer for rendering the data that is along a route.
@@ -18,14 +17,14 @@ function GetMap() {
     Microsoft.Maps.Events.addHandler(dataLayer, 'click', shapeClicked);
 
     //Create an infobox at the center of the map but don't show it.
-    infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
-        visible: false,
+    /* infobox = new Microsoft.Maps.Infobox(map.getCenter(), {
+        visible: true,
         title: 'Carrefour',
         description: ''
-    });
+    }); */
 
     //Assign the infobox to a map instance.
-    infobox.setMap(map);
+    //infobox.setMap(map);
 
     //Load the directions and spatial data service modules.
     Microsoft.Maps.loadModule(['Microsoft.Maps.Directions', 'Microsoft.Maps.SpatialDataService'], function() {
@@ -48,11 +47,6 @@ function GetMap() {
 
 
 
-
-
-
-
-
     /* Desenho */
 
     Microsoft.Maps.loadModule('Microsoft.Maps.DrawingTools', function() {
@@ -61,19 +55,23 @@ function GetMap() {
 
         //Show the drawing toolbar and enable editting on the map.
         tools.showDrawingManager(function(manager) {
-            //Store a reference to the drawing manager as it will be useful later.
-            drawingManager = manager;
 
-            //Create a shortcut to the DrawingBarAction to minimize code.
-            var da = Microsoft.Maps.DrawingTools.DrawingBarAction;
-
-            //Limit which tools appear in the drawing toolbar.
-            manager.setOptions({
-                drawingBarActions: da.polyline | da.polygon | da.erase,
-                fillColor: 'rgba(255, 100, 0, 0.5)'
-            });
-        });
+            //Add events to the drawing manager.
+            Microsoft.Maps.Events.addHandler(manager, 'drawingChanged', function() { highlight('drawingChanged'); });
+            Microsoft.Maps.Events.addHandler(manager, 'drawingChanging', function() { highlight('drawingChanging'); });
+            Microsoft.Maps.Events.addHandler(manager, 'drawingEnded', function() { highlight('drawingEnded'); });
+            Microsoft.Maps.Events.addHandler(manager, 'drawingErased', function() { highlight('drawingErased'); });
+            Microsoft.Maps.Events.addHandler(manager, 'drawingModeChanged', function() { highlight('drawingModeChanged'); });
+            Microsoft.Maps.Events.addHandler(manager, 'drawingStarted', function() { highlight('drawingStarted'); });
+        })
     });
+
+    function highlight(id) {
+        //Highlight the mouse event div to indicate that the event has fired.
+
+        //Remove the highlighting after a second.
+
+    }
 
     /* Desenho */
 
@@ -104,13 +102,13 @@ function directionsUpdated(e) {
 
     //Create a query to get nearby data.
     var queryOptions = {
-        queryUrl: sdsDataSourceUrl,
+        /* queryUrl: sdsDataSourceUrl, */
         spatialFilter: {
             spatialFilterType: 'nearRoute',
             start: currentRoute.routeLegs[0].startWaypointLocation,
             end: currentRoute.routeLegs[0].endWaypointLocation,
             travelMode: getRouteMode(routeRequest),
-            optimize: getRouteOptimization(routeRequest)
+            optimize: getRouteOptimization(routeRequest),
         }
     };
 
