f87be61fcfebd3ea688b4dbb4751f941b1eec891
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2021-07-05 01:32:49 -0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,5 +1,9 @@
 var caixaText = document.getElementById('caixaText')
 
+
+var campos = document.getElementsByTagName('input')
+
+
 var shif = document.getElementById('shif')
 
 onkeydown = (e) => {
@@ -13,9 +17,21 @@ onkeydown = (e) => {
 }
 
 shif.onclick = () => {
-    if (caixaText.style.display == "none") {
-        caixaText.style.display = "block"
-    } else {
-        caixaText.style.display = "none"
+    document.getElementsByTagName('input')[19].value = 'Avenida Professor Francisco Morato 2585, São Paulo, 05513-300, Brasil' /* Depois irei usar o Select */
+
+    for (let i = 0; i < 20; i++) {
+
+        let valor = `São Paulo, ${caixaText.value.split('\n')[i]}, Brasil`
+
+        campos[i + 20].value = valor.trim()
+
+
     }
+
+
+    /*  if (caixaText.style.display == "none") {
+         caixaText.style.display = "block"
+     } else {
+         caixaText.style.display = "none"
+     } */
 }
