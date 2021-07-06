eba5f9cd43c7a602aadf326a20a2477e8195992c
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2021-07-06 20:06:55 -0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,40 +1,44 @@
+/* Não é Mapa */
+
 var caixaText = document.getElementById('caixaText')
 
 
 var campos = document.getElementsByTagName('input')
 
+var btnDeletar = document.getElementById('btnDeletar')
+var btnOk = document.getElementById('btnOk')
+var btnMostrar = document.getElementById('btnMostrar')
 
-var shif = document.getElementById('shif')
+function mostarOcultar() {
+    if (caixaText.style.display == "none") {
+        caixaText.style.display = "block"
+    } else {
+        caixaText.style.display = "none"
+    }
+}
+
+function limparCampos() {
+    for (let i = 0; i < 30; i++) { campos[i + 20].value = "" }
+
+    map.entities.clear();
+}
+
 
 onkeydown = (e) => {
-    if (e.ctrlKey && e.shiftKey) {
-        if (caixaText.style.display == "none") {
-            caixaText.style.display = "block"
-        } else {
-            caixaText.style.display = "none"
-        }
-    }
-    if (e.ctrlKey && e.code == 'Delete') {
-        for (let i = 0; i < 30; i++) {
-            campos[i + 20].value = ""
-        }
-    }
+    if (e.ctrlKey && e.shiftKey) { mostarOcultar() }
+    if (e.ctrlKey && e.code == 'Delete') { limparCampos() }
 }
 
 
 
-shif.onclick = () => {
+btnMostrar.onclick = () => { mostarOcultar() }
+btnDeletar.onclick = () => { limparCampos() }
+
+btnOk.onclick = () => {
     document.getElementsByTagName('input')[19].value = 'Avenida Professor Francisco Morato 2585, São Paulo, 05513-300, Brasil' /* Depois irei usar o Select */
 
     for (let i = 0; i < 30; i++) {
-
         let valor = `${caixaText.value.split('\n')[i]}`
-
         campos[i + 20].value = valor.trim()
-
-
     }
-
-
-
 }
