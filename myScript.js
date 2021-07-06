5ba031951850979211b9bfd613e499ba1cb172cf
Leandro Cullen <73318882+appwhat@users.noreply.github.com>
2021-07-06 12:54:42 -0300

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -14,12 +14,19 @@ onkeydown = (e) => {
             caixaText.style.display = "none"
         }
     }
+    if (e.ctrlKey && e.code == 'Delete') {
+        for (let i = 0; i < 30; i++) {
+            campos[i + 20].value = ""
+        }
+    }
 }
+
+
 
 shif.onclick = () => {
     document.getElementsByTagName('input')[19].value = 'Avenida Professor Francisco Morato 2585, São Paulo, 05513-300, Brasil' /* Depois irei usar o Select */
 
-    for (let i = 0; i < 20; i++) {
+    for (let i = 0; i < 30; i++) {
 
         let valor = `${caixaText.value.split('\n')[i]}`
 
@@ -29,9 +36,5 @@ shif.onclick = () => {
     }
 
 
-    /*  if (caixaText.style.display == "none") {
-         caixaText.style.display = "block"
-     } else {
-         caixaText.style.display = "none"
-     } */
+
 }
